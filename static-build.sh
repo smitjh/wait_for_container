d5363f208cd5414707c990ef04b7deb8269b9cff
Jay Smit <jay.smit@cohesic.com>
2023-06-20 15:39:43 -0600

    Make upx optional

--- static-build.sh
+++ static-build.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-# Expects musl and upx to be installed. 
+# Build a statically linked executable using musl.
+# This should produce a file that should be portable to any linux system.
+
+# Expects musl to be installed to /usr/local/musl
 export PATH=$PATH:/usr/local/musl/bin
 nimble build --gcc.exe:musl-gcc --gcc.linkerexe:musl-gcc --passL:-static --opt:size -d:release
 strip -s wait_for_container
-upx --best wait_for_container
+
+if [ "$(which upx)" != "" ] ; then
+    upx --best wait_for_container
+else
+    echo "Upx not installed. Consider installing it to further reduce binary size"
+    echo "See: https://upx.github.io/"
+fi
